e8bb4356e977c39b94a2fcdad5c56503c512fb42
Paweł Marko <pmarko@qualio.com>
2022-10-31 00:39:22 +0100

    Adding web adapter for users

---
+++ src/adapters/into/web/users.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends, Response
+from fastapi import status as status_code
+
+from adapters.out.db.db_connection import create_session
+from adapters.out.db.users_repository import UsersRepository
+from application.use_case import RegisterUserUsecase
+from domain.users import User, UserCreate
+from utils import get_current_user
+
+router = APIRouter()
+
+
+@router.post(
+    "",
+    response_model=User,
+    status_code=status_code.HTTP_201_CREATED,
+    tags=["users"],
+)
+async def register_user(user: UserCreate):
+    async with create_session() as dbsession:
+        use_case = RegisterUserUsecase(UsersRepository(dbsession))
+        result = await use_case.execute(
+            {"username": user.name, "password": user.password}
+        )
+        return result
+
+
+@router.get(
+    "",
+    response_model=User,
+    status_code=status_code.HTTP_200_OK,
+    tags=["users"],
+)
+async def get_user(
+    current_user: User = Depends(get_current_user),
+):
+    return current_user
